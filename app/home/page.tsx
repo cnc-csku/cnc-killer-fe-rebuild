fde14847d40835fd1af3bb92c533c19550401576
Pannathorn Suetrong <pannathorn.su@ku.th>
2025-04-14 17:00:05 +0700

    add more information to home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,6 +4,11 @@ import Lottie, { useLottie } from "lottie-react"
 import Punch from "@/public/animations/punch.json"
 import { Button } from "@heroui/button"
 import { useState } from "react"
+import { Mitr } from 'next/font/google'
+const mitr = Mitr({
+  weight: ['600'],
+  subsets: ['latin'],
+})
 
 export default function HomePage(){
     const [play , setIsPlay] = useState(false)
@@ -11,15 +16,15 @@ export default function HomePage(){
     const LottieObj = useLottie({
         animationData: Punch,
         style: {
-            height: "50vh",
-            width: "50vw",
+            height: "40wh",
+            width: "40vw",
         },
         autoplay: false,
         loop: false
     })
     return <>
-    <div className="flex flex-col w-full mt-5 justify-center items-center">
-        <div className="text-white text-2xl p-0 m-0">test</div>
+    <div className="flex flex-col w-full justify-center items-center">
+        <div className="text-white text-2xl p-0 m-0">PLAYER NAME</div>
     <div className="p-0 m-0">{LottieObj.View}</div>
     <Button onPress={() => {
         if (play) {
@@ -29,6 +34,14 @@ export default function HomePage(){
         LottieObj.play()
         setIsPlay(true)
     }}>press me</Button>
+    </div>
+    <div className="w-full flex flex-col mt-3 items-center space-y-1 text-white text-xl">
+            <div>YOUR VICTIM :</div>
+            <div className={`${mitr.className} text-green-500`}>NAME</div>
+            <div>ACTION : </div>
+            <div className={` ${mitr.className} text-red-500`}>ACTION NAME</div>
+            <div>YOUR CONDITION : </div>
+            <div className={` ${mitr.className} text-red-500`}>ACTION NAME</div>
     </div>
     </>
 
